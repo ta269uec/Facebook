cd2865e1db4b82adb6205fef7976a4399b05d7c9
TarunJoshi <TarunJoshi@10.0.0.81>
2018-05-06 18:04:06 -0400

    Code to generate all the views we require to answer our queries.

--- common.py
+++ common.py
@@ -4,3 +4,11 @@
 def read_access_token_from_file(local_file_name):
     with open(local_file_name, "r") as fid:
         return fid.readline()
+    
+def convert_time_zone(from_date_time, from_zone='UTC', to_zone='America/New_York'):
+    from datetime import datetime
+    from dateutil import tz
+    from_zone, to_zone = tz.gettz(from_zone), tz.gettz(to_zone)
+    # Tell the datetime object that it's in from_zone time zone since datetime objects are 'naive' by default    
+    from_date_time = from_date_time.replace(tzinfo=from_zone)
+    return from_date_time.astimezone(to_zone)    
--- my_posts.py
+++ my_posts.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 from sys import exit
 from pprint import pprint
 from collections import defaultdict
+from copy import deepcopy
 
 from facepy import GraphAPI
 from facepy.utils import get_extended_access_token
 
-from common import read_access_token_from_file
+from common import read_access_token_from_file, convert_time_zone
 
 class MyPosts(object):
     # The required post fields, post edges, and options are defined here. 
@@ -17,6 +19,7 @@ class MyPosts(object):
                             'attachments':set(['type','title','description'])     
                         }
     required_options = {'include_hidden':True}
+    
     def __init__(self, graph_api, fields=None, post_edges=None, options=None):
         self.me_id = self.__get_me_id(graph_api)
         if self.me_id is None:
@@ -32,7 +35,16 @@ class MyPosts(object):
                 else:
                     temp[k] = post_edges[k]
             post_edges = temp
-        options = MyPosts.required_options if options is None else required_options
+        if options is None:            
+            options = MyPosts.required_options 
+        else:
+            temp = deepcopy(MyPosts.required_options)
+            for k,v in options.items():
+                temp[k] = v
+            options = temp
+        
+#         pprint(options)
+#         exit()
         self.posts = graph_api.get(self.__build_query_string(fields, post_edges, options))['data']
         post_id_view, reaction_view = self.__build_post_views(self.posts)
         return
@@ -49,32 +61,43 @@ class MyPosts(object):
     def __build_query_string(self, fields, post_edges, options):
         query_params_str = ','.join(fields) if fields else ""
         edges_str = ','.join(["%s{%s}"%(k, ','.join(v)) for k,v in post_edges.items()]) if post_edges else ""
-        options_str = ','.join(["%s=%s"%(k,str(v)) for k,v in options.items()]) if options else ""
+        options_str = '&'.join(["%s=%s"%(k,str(v)) for k,v in options.items()]) if options else ""
         query_string = ''.join(['me/posts', '?fields=', query_params_str, ',', edges_str, '&', options_str])
         return query_string
     
     def __build_post_views(self, posts):
+        """
+            In a single pass, build out views which can help us answer queries on this data.
+        """
         post_id_view = {} # Key is post_id and value is the post
         reaction_id_view = {} # Key is reaction id and value is post id
         reaction_type_view = defaultdict(list) # Key is reaction_type and value consists of a list of post ids.
         # Key is friend's name and value is a dictionary which has key as reaction type and value as list of post-ids where they have reacted.
-        friend_reaction_view = defaultdict(dict) 
+        friend_reaction_view = defaultdict(dict)
+        # key is year, value is dictionary with keys as month and values as list of posts in that year-month
+        year_month_view = defaultdict(dict)
+        # key is weekday and value is a dictioanry with key as hour and value as list of posts in that weekday and hour
+        weekday_hour_view = defaultdict(dict)
         
+        WEEKDAYS = ["Mon", "Tues", "Wed", "Thrus", "Fri", "Sat", "Sun"]
         for post in posts:
-            if 'from' in posts and posts['from']['id'] ==  self.me_id:
-                post_id_view[post['id']] = post
-                if "reactions" in post:
-                    for reaction in post["reactions"]["data"]:
-                        reaction_id_view[reaction['id']] = post['id']
-                        if 'type' in reaction:
-                            reaction_type_view[reaction['type']].append(post['id'])
-                            if 'name' in reaction and 'profile_type' in reaction and  reaction['profile_type'] == 'user':
-                                friend_reaction_view[reaction['name']][reaction['type']].append(post['id'])
+#             if 'from' in post and post['from']['id'] ==  self.me_id:
+            post_id_view[post['id']] = post
+            if 'created_time' in post:
+                utc_created_time = datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
+                est_created_time = convert_time_zone(from_date_time=utc_created_time, from_zone='UTC', to_zone='America/New_York')
+                year, month, weekday, hour, minute = est_created_time.year, est_created_time.month, WEEKDAYS[est_created_time.weekday()], est_created_time.hour, est_created_time.minute 
+                year_month_view[year].setdefault(month, []).append(post['id'])
+                weekday_hour_view[weekday].setdefault(hour, []).append(post['id'])
+            if "reactions" in post:
+                for reaction in post["reactions"]["data"]:
+                    reaction_id_view[reaction['id']] = post['id']
+                    if 'type' in reaction:
+                        reaction_type_view[reaction['type']].append(post['id'])
+                        if 'name' in reaction and 'profile_type' in reaction and  reaction['profile_type'] == 'user':
+                            friend_reaction_view[reaction['name']].setdefault(reaction['type'], []).append(post['id'])
         return post_id_view, reaction_id_view
     
-
-#  'created_time': '2018-04-05T01:45:54+0000',
-
 if __name__ == '__main__':
     # Paste your app token in a local file on your machine and add its path here.
     # Note this token is a user token and is temporary. This will expire in 2-3 hours and you need to regenrate it.
@@ -85,13 +108,12 @@ if __name__ == '__main__':
     graph_api = GraphAPI(ACCESS_TOKEN)
     
     
-    post_fields = ['created_time', 'from', 'caption', 'timeline_visibility', 'privacy', 'message', 'place', 'story', 'with_tags']
-    post_edges = {
+    additional_post_fields = ['created_time', 'from', 'caption', 'timeline_visibility', 'privacy', 'message', 'place', 'story', 'with_tags']
+    additional_post_edges = {
                     'reactions':['type','profile_type','username','name'],
                     'comments':['from','message','like_count','comment_count', 'created_time'],
                     'attachments':['type','title','description']                                        
                  }
-    options = {'include_hidden':True}
-#     my_posts = MyPosts(graph_api, post_fields, post_edges, options)
-    my_posts = MyPosts(graph_api)    
+    additional_options = {'include_hidden':True, 'limit':"1000000"} # TODO: Implement pagination using a generator
+    my_posts = MyPosts(graph_api, options=additional_options)    
     pprint(my_posts.posts)
